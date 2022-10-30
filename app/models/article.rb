9d36e02b504ffa5552c9d9da4315a6c06d21ebf2
Mutuma Gitonga <mgitonga150@gmail.com>
2022-10-31 02:08:02 +0300

    add object initialization, getters & setters, & object relationship methods

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,19 @@
-class Article
 
+class Article
+  attr_reader :author, :magazine, :title
+  @@all = []
+
+  def initialize(author, magazine, title)
+    @author = author.name
+    @magazine = magazine.name
+    @title = title
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 
 end
+
+
--- tools/console.rb
+++ tools/console.rb
@@ -4,6 +4,21 @@ def reload
   load 'config/environment.rb'
 end
 
+mag1 = Magazine.new("nation", "political")
+mag2 = Magazine.new("star", "business")
+mag3 = Magazine.new("standard", "editorial")
+
+
+author1 = Author.new("John")
+author2 = Author.new("Peter")
+author3 = Author.new("Kevin")
+
+art1 = Article.new(author1, mag1, "Elections 2022")
+art2 = Article.new(author2, mag2, "Mombase Port")
+art3 = Article.new(author1, mag1, "DJ Trump")
+art4 = Article.new(author1, mag3, "Ukraine War Crisis")
+art5 = Article.new(author3, mag3, "Standard's 2nd author")
+
 
 ###  WRITE YOUR TEST CODE HERE ###
 
